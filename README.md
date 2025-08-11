fc86f328618fa9155ddd82a39582f86c7604f8f8
Memory77 <soccio.deborah@gmail.com>
2025-08-11 13:45:59 +0200

    modif readme+test

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+Projet NLP GAME V1<br><br><br><br>
 version du nlp_game antérieur, juste avec la generation de text sans le speech text et le text speech. 
 
 Prérequis : Python3
@@ -5,11 +6,11 @@ Prérequis : Python3
 - cloner le projet
 - creer un .env : 
 
-OPENAI_API_KEY= ta clé 
+OPENAI_API_KEY= ta clé <br>
 OPENAI_MODEL= exemple gpt-3.5-turbo
 
 - créer un environnement virtuel et l'activer : python3 -m venv venv + source venv/bin/activate(linux)
-- installer les dépendances : pip install r requirements.txt
+- installer les dépendances : pip install -r requirements.txt
 - python3 triv_poursuite.py -> pour lancer une db vide
 - sqlite3 triv_ia_dlc.db < INSERT.sql
 - python3 triv_poursuite.py
--- test_openai.py
+++ test_openai.py
@@ -2,25 +2,14 @@ import os
 import openai
 from dotenv import load_dotenv
 
-# Charger les variables d'environnement à partir du fichier .env
 load_dotenv()
 
-# Récupérer les valeurs des variables d'environnement
-api_key = os.getenv('openai_api_key')
-api_base = os.getenv('openai_api_key_base')
-api_deployment = os.getenv('openai_api_deployment')
-api_version = os.getenv('openai_api_version')
+# Clé OpenAI standard
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Configurer l'API OpenAI avec les informations d'Azure
-openai.api_key = api_key
-openai.api_base = api_base
-openai.api_type = 'azure'
-openai.api_version = api_version
-
-# Fonction pour appeler l'API et obtenir une réponse
 def get_response(prompt):
     response = openai.ChatCompletion.create(
-        engine=api_deployment,
+        model="gpt-3.5-turbo",   # ou "gpt-4o", "gpt-4"
         messages=[
             {"role": "system", "content": "You are an orc."},
             {"role": "user", "content": prompt}
@@ -29,7 +18,5 @@ def get_response(prompt):
     )
     return response.choices[0].message['content'].strip()
 
-# Exemple d'utilisation de la fonction
 prompt = "What is the capital of France?"
-response = get_response(prompt)
-print(response)
+print(get_response(prompt))
